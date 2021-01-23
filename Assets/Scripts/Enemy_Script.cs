e953c2f579da123361ef0f78183a441728eb27aa
Mustan Pathetic <patheticmustan@gmail.com>
2021-01-23 15:45:45 -0500

    eeee fix enemy clipping walls

--- Assets/Scripts/Enemy_Script.cs
+++ Assets/Scripts/Enemy_Script.cs
@@ -2,37 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Script : MonoBehaviour
-{
+public class Enemy_Script : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
-    public float speed;
+    public float moveSpeed;
     public Transform target;
-    public bool overlapping;
-    void Start()
-    {
+    // public bool overlapping;
+    void Start() {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        
 
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 3;
     }
 
-   
-    void Update()
-    {
-        if (overlapping == true)
-        {
 
-            speed = 1;
-        }
+    void FixedUpdate() {
+        // ah yes, broken code
+        //movement = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        //rb.MovePosition(rb.position + movement);
 
-
-
-
-
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
+        // move towards the player pos
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
-  
-    
 }
 
 
