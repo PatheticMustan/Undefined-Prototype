2f45d149e6a2dc82f4487b46367dc5afa412dfa5
Mustan Pathetic <patheticmustan@gmail.com>
2021-01-30 11:08:56 -0500

    crates, incomplete

---
+++ Assets/CrateScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CrateScript : MonoBehaviour
+{
+    public GameObject wallTiles;
+
+    void Start()
+    {
+        wallTiles.GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") { 
+            
+        }
+    }
+}
