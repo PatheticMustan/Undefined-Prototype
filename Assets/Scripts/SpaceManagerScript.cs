7041e58df726cc300b6a5a10360ba5fd09cb022d
Mustan Pathetic <patheticmustan@gmail.com>
2021-02-20 14:33:48 -0500

    comments in spaceManager

--- Assets/Scripts/SpaceManagerScript.cs
+++ Assets/Scripts/SpaceManagerScript.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceManagerScript : MonoBehaviour
-{
+public class SpaceManagerScript : MonoBehaviour {
     // a boolean to denote if there is an impassable wall
     public bool[][] passable;
+
+    public delegate void multiDelegateTrigger();
     // triggers for buttons, possibly cutscenes...?
-    public bool[][] triggers;
+    public multiDelegateTrigger[,] triggers;
 
-    public delegate void CallbackDelegate();
-    public CallbackDelegate
-
-    void Start()
-    {
-        
+    void Start() {
+        // loadLevel();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
-    public void registerTrigger(int x, int y, delegate callback) { 
-        triggers
+    public void loadLevel(int width, int length) {
+        triggers = new multiDelegateTrigger[width, length];
+    }
+
+    public multiDelegateTrigger getTrigger(int x, int y) {
+        return triggers[x, y];
     }
 }
