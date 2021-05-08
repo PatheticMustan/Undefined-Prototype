311d9eef0b5dc7212deebc92f4f97909f6bc45fc
Mustan Pathetic <patheticmustan@gmail.com>
2021-05-08 15:47:22 -0400

    add doors and keys to level 2

--- Assets/Scripts/ButtonPushScript.cs
+++ Assets/Scripts/ButtonPushScript.cs
@@ -2,45 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonPushScript : MonoBehaviour
-{
+public class ButtonPushScript : MonoBehaviour {
 
     public GameObject door;
     public bool open;
 
-    void Start()
-    {
+    void Start() {
         open = false;
     }
 
 
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
+    void OnTriggerEnter2D(Collider2D col) {
 
-        if ((col.gameObject.tag.Equals("Crate") || col.gameObject.tag.Equals("Player")))
-            {
-
+        if (col.gameObject.tag.Equals("Crate") || col.gameObject.tag.Equals("Player")) {
             open = !open;
             door.SetActive(!open);
             GetComponent<SpriteRenderer>().color = open ? new Color(0x00, 0xFF, 0x00) : new Color(0xFF, 0x00, 0x00);
         }
-      
+
     }
-    void OnTriggerExit2D(Collider2D col)
-    {
-        if ((col.gameObject.tag.Equals("Crate") || col.gameObject.tag.Equals("Player")))
-        {
+    void OnTriggerExit2D(Collider2D col) {
+        if ((col.gameObject.tag.Equals("Crate") || col.gameObject.tag.Equals("Player"))) {
             OnTriggerEnter2D(col);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Crate")) 
-        {
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag.Equals("Crate")) {
             open = false;
         }
     }
 }
-
