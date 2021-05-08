b9b344a9b2323973d6c8979f886d40cd8d56566f
Mustan Pathetic <patheticmustan@gmail.com>
2021-05-08 12:34:29 -0400

    basic enemy path connector

---
+++ Assets/EnemyPathConnector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnemyPathConnector : MonoBehaviour {
+    public GameObject[] points;
+
+    public void OnValidate() {
+        Debug.Log("ahhh! you are valid!");
+        Debug.Log(points.Length);
+    }
+}
