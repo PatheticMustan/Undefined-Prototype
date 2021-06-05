c21d6b5b155056f5a8eac0745ae94377ee7dc24b
RedWool <leolui0930.sia@gmail.com>
2021-06-05 13:59:39 -0400

    Particle and Other Changes WOhojo
    Box Destroy Particle added + fixed sprite layering issues

---
+++ Assets/BoxDestroyPS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDestroyPS : MonoBehaviour
+{
+    public GameObject boxObj;
+
+    public ParticleSystem boxPS;
+
+    private bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!boxObj.activeSelf && !flag)
+        {
+            flag = true;
+
+            boxPS.gameObject.transform.position = boxObj.transform.position;
+
+            boxPS.Play();
+        }
+    }
+}
